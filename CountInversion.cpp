45531d998d407c5e4db09af53d9739a303ac18db
Chinmoy Mondal <5gsanmoy@gmail.com>
2022-06-25 22:03:58 +0530

    Day 2 Striver SDE Sheet

--- CountInversion.cpp
+++ CountInversion.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int merge_(int a[],int temp[],int left,int mid,int right){
+/*int merge_(int a[],int temp[],int left,int mid,int right){
     int count=0;
     int i = left;
     int j = mid;
@@ -23,25 +23,77 @@ int merge_(int a[],int temp[],int left,int mid,int right){
         a[i]=temp[i];
     }
     return count;
+}*/
+
+int Merge(int a[],int left,int mid,int right){
+    int count=0;
+    int n1=mid-left+1;
+    int n2=right-mid;
+    int b[n1],c[n2];
+    for(int i=0;i<n1;i++){
+        b[i]=a[left+i];
+    }
+    for(int i=0;i<n2;i++){
+        c[i]=a[mid+1+i];
+    }
+    int i=0;
+    int j=0;
+    int k=left;
+
+    while(i<n1 && j<n2){
+        if(b[i]<=c[j]){
+            a[k]=b[i];
+            //cout<<a[k]<<" ";
+            k++,i++;
+        }else{
+            a[k]=c[j];
+            //cout<<a[k]<<" ";
+            k++,j++;
+            count=count+(n1-i);
+            
+        }
+    }
+    while(i<n1)
+        a[k++]=b[i++];
+    while(j<n2)
+        a[k++]=c[j++];
+
+
+    //cout<<count<<" ";
+
+
+    return count;
+
 }
 
-int mergeSort(int a[],int temp[],int left,int right){
+int mergeSort(int a[],int left,int right){
     int mid,count=0;
     if(right>left){
         mid=(right+left)/2;
-        count+=mergeSort(a,temp,left,mid);
-        count+=mergeSort(a,temp,mid+1,right);
-        count+=merge_(a,temp,left,mid+1,right);
+        count+=mergeSort(a,left,mid);
+        count+=mergeSort(a,mid+1,right);
+        count+=Merge(a,left,mid,right);
     }
+    
     return count;
 }
 
+/*int mergeSort_(int a[],int temp[],int left,int right){
+    int mid,count=0;
+    if(right>left){
+        mid=(right+left)/2;
+        count+=mergeSort_(a,temp,left,mid);
+        count+=mergeSort_(a,temp,mid+1,right);
+        count+=merge_(a,temp,left,mid+1,right);
+    }
+    return count;
+}*/
 int main(){
 
     cout<<"Array Size:";
     int n;cin>>n;
     cout<<"Array Elements:\n";
-    int a[n],temp[n];
+    int a[n];//,temp[n];
     //vector<int> a;
     for(int i=0;i<n;i++){
         cin>>a[i];
@@ -49,8 +101,11 @@ int main(){
         //a.emplace_back(x);
     }
     //vector<int> temp;
-    int ans=mergeSort(a,temp,0,n-1);
-    cout<<"The total inversions are "<<ans<<endl; 
+    // int ans=mergeSort_(a,temp,0,n-1);
+    // cout<<"The total inversions are "<<ans<<endl; 
+
+    int ans1=mergeSort(a,0,n-1);
+    cout<<"The total inversions are "<<ans1<<endl;
 
     return 0;
 }
