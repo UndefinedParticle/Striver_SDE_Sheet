3c61613bdca9421546f516bff1acb57ad5760b09
Chinmoy Mondal <5gsanmoy@gmail.com>
2022-12-08 23:43:19 +0530

    Striver SDE Sheet

--- MaximumWidthOfBinaryTree.cpp
+++ MaximumWidthOfBinaryTree.cpp
@@ -4,11 +4,46 @@ using namespace std;
 
 
 
-struct node{
+struct TreeNode{
     int data;
-    node *left,*right;
+    TreeNode *left,*right;
 };
 
+int maxWidth(TreeNode *root){
+
+    if(root==NULL){
+        return 0;
+    }
+    int ans=0;
+    queue<pair<TreeNode*,int>> q;
+    q.push({root,0});
+    while(!q.empty()){
+        int size=q.size();
+        int curMin=q.front().second;
+        int leftMost,rightMost;
+        loop(i,0,size){
+            int curId=q.front().second - curMin;
+            TreeNode *temp=q.front().first;
+            q.pop();
+            if(i==0){
+                leftMost=curId;
+            }
+            if(i==size-1){
+                rightMost=curId;
+            }
+            if(temp->left){
+                q.push({temp->left,2*curId+1});
+            }
+            if(temp->right){
+                q.push({temp->right,2*curId +2});
+            }
+        }
+        ans=max(ans,rightMost-leftMost+1);
+    }
+    
+
+    return ans;
+}
 
 
 
